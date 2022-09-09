e695ef7d8d09e2a310fd33270946461e95ad215b
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-09-09 09:17:06 -0300

    Terceira semana

--- Segunda semana/Aula6-MesaDeTrabalho_G/src/components/Home.jsx
+++ Segunda semana/Aula6-MesaDeTrabalho_G/src/components/Home.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 
-/* Importando o módulo CSS */
-import styleModule from './home.module.css';
+import City from './City';
 
 /* Componente do tipo funcional */
 function HomeComponent(props) {
     return (<div>
-        {props.items.map((item) => {
-            if (item.country == "BRA") {
-                return <React.Fragment key={item.id} >
-                    <h2>ID {item.id}</h2>
-                    {/* CSS InLine: Aproveitando a cor disponibilizada no próprio objeto JS */}
-                    <h1 style={{ color: item.color }}>{item.city}</h1>
-                    <h3>{item.country}</h3>
-                    {/* CSS Module */}
-                    <p className={styleModule.fonte}>{item.population}</p>
-                </React.Fragment>
+        {props.items.map((itemIteracao) => {
+            if (itemIteracao.country == "BRA") {
+                
+                /* Retorna o item/objeto de cada iteração para o componente "City" que constroi o componente */
+                return <City item={itemIteracao} />
             }
         })}
     </div>)

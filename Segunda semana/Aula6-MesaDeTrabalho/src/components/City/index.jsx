9fc7c4a3a5109a3fad11e096c76c5818ed17811f
Thribs <thribsilva@gmail.com>
2022-10-26 20:53:15 -0300

    Added missing file in previous commit

---
+++ Segunda semana/Aula6-MesaDeTrabalho/src/components/City/index.jsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.css'
+
+const City = ({ id, country, name, population, color }) =>
+    <>
+    </>
+
+export default City
+
+// {
+//     id: 1,
+//     country: "BRA",
+//     city: "São Paulo",
+//     population: "12.396.372",
+//     color: "#A440E2"
+//   },
